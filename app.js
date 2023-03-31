b23b8118c9512ea17bc6d0a3b6795cbf29783acb
createdbybenlinn <benlinn26@gmail.com>
2023-03-31 14:52:58 -0700

    save afternoon mongoose

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const { PORT = 3001 } = process.env;
 const app = express();
+mongoose.connect("mongodb://localhost:27017/wtwr_db");
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
 });
