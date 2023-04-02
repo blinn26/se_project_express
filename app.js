03dcd993eadb83946f511c54335de1af087749c3
createdbybenlinn <benlinn26@gmail.com>
2023-04-02 09:33:41 -0700

    added folders to project

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { PORT = 3001 } = process.env;
 const app = express();
-mongoose.connect("mongodb://localhost:27017/wtwr_db");
+mongoose.connect("mongodb://localhost:27`017/wtwr_db");
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
