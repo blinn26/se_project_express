d128ddc389171e9aaa64f6c8c341645eb628af21
createdbybenlinn <benlinn26@gmail.com>
2023-04-03 10:21:29 -0700

    fixed itemrouter name

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const itemRouter = require("./itemRouter");
+const clothingItem = require("./clothingItem");
 
-router.use("/items", itemRouter);
+router.use("/items", clothingItem);
 
 router.use((req, res) => {
   res.status(500).send({ message: "Router Not found" });
 });
+
+module.exports = router;
