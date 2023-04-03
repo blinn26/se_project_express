a43358ad140a535c579592c2f833b74d82563ef7
createdbybenlinn <benlinn26@gmail.com>
2023-04-03 09:05:03 -0700

    committtttt

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const { PORT = 3001 } = process.env;
 const app = express();
-mongoose.connect('mongodb://localhost:27`017/wtwr_db');
+mongoose
+  .connect("mongodb://localhost:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+  console.log("This is the terminal and Yes this is Working.");
 });
