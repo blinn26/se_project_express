5dce64fd7d8cbb993d09c20fca61a3ecbe60e960
createdbybenlinn <benlinn26@gmail.com>
2023-04-03 21:08:41 -0700

    name spelling

--- routes/clothingitem.js
+++ routes/clothingitem.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-const { createItem } = require("../controllers/clothingitem");
+const { createItem } = require("../controllers/clothingItem");
 
 //CRUD
 
