cd47d66a98c7c3ffdc72907c352c101141daae1c
createdbybenlinn <benlinn26@gmail.com>
2023-04-04 14:31:46 -0700

    app crashing

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,41 @@
+const User = require("../models/user");
 
+const getUsers = (req, res) => {
+  User.find()
+    .then((users) => {
+      res.status(200).send({ data: users });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error from getUsers", err });
+    });
+};
+
+const getUser = (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((user) => {
+      res.status(200).send({ data: user });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error from getUser", err });
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar })
+    .then((user) => {
+      res.status(200).send({ data: user });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error from createUser", err });
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const clothingItem = require("./clothingItem");
+const user = require("./user.js"); // Import user routes
+// Import user routes
 
 router.use("/items", clothingItem);
+router.use("/users", user); // Add user routes
 
 router.use((req, res) => {
-  res.status(500).send({ message: "Router Not found" });
+  res.status(404).send({ message: "Requested resource not found" });
 });
 
 module.exports = router;
