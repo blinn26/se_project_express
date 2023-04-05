8eb5f490c3b1db6248120f112f1de0978c7c3df6
createdbybenlinn <benlinn26@gmail.com>
2023-04-05 15:53:01 -0700

    post

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { getUsers, getUser, createUser } = require("../controllers/user");
+const { getUsers, getUser, createUser } = require("../controllers/users");
 
 router.get("/", getUsers);
 router.get("/:userId", getUser);
