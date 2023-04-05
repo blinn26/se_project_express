619684f2b89f64124d6f187e746c9e0fec6dee24
createdbybenlinn <benlinn26@gmail.com>
2023-04-04 22:19:26 -0700

    update on fail

--- controllers/clothingitem.js
+++ controllers/clothingitem.js
@@ -26,9 +26,14 @@ const updateItem = (req, res) => {
   const { itemId } = req.params;
   const { imageURL } = req.body;
 
-  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
-    .orFail()
-    .then((item) => res.status(200).send({ data: item }))
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } }, { new: true })
+    .then((item) => {
+      if (!item) {
+        res.status(404).send({ message: "Item not found" });
+      } else {
+        res.status(200).send({ data: item });
+      }
+    })
     .catch((err) => {
       res.status(500).send({ message: "Error from updateItem", err });
     });
@@ -38,35 +43,16 @@ const deleteItem = (req, res) => {
   const { itemId } = req.params;
 
   ClothingItem.findByIdAndDelete(itemId)
-    .orFail()
-    .then(() => res.status(204).send({}))
+    .then((item) => {
+      if (!item) {
+        res.status(404).send({ message: "Item not found" });
+      } else {
+        res.status(204).send({});
+      }
+    })
     .catch((err) => {
       res.status(500).send({ message: "Error from deleteItem", err });
     });
-};
-
-// clothingItem.controller.js
-
-const ERROR_CODES = require("../utils/errors");
-
-module.exports.createClothingItem = async (req, res, next) => {
-  try {
-    // ... code to create clothing item ...
-    res.status(201).json(clothingItem);
-  } catch (err) {
-    err.statusCode = ERROR_CODES.BAD_REQUEST;
-    next(err);
-  }
-};
-
-module.exports.getClothingItem = async (req, res, next) => {
-  try {
-    // ... code to get clothing item ...
-    res.json(clothingItem);
-  } catch (err) {
-    err.statusCode = ERROR_CODES.NOT_FOUND;
-    next(err);
-  }
 };
 
 module.exports = {
