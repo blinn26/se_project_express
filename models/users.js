625b66630b16d40595d3f170a719d11197b1e011
createdbybenlinn <benlinn26@gmail.com>
2023-04-10 14:04:11 -0700

    schema

--- models/users.js
+++ models/users.js
@@ -7,13 +7,31 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 2,
     maxlength: 30,
+    default: "Elise Bouer",
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: function (v) {
+        return validator.isEmail(v);
+      },
+      message: "{VALUE} is not a valid email address",
+    },
+  },
+  password: {
+    type: String,
+    required: true,
   },
   avatar: {
     type: String,
     required: true,
+    default:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png",
     validate: {
-      validator: function validateURL(value) {
-        return validator.isURL(value);
+      validator: function (v) {
+        return validator.isURL(v);
       },
       message: "{VALUE} is not a valid URL",
     },
