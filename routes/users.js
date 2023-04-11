725871f6a108771193377261b66703cebdefc034
createdbybenlinn <benlinn26@gmail.com>
2023-04-11 16:18:10 -0700

    removed express

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,3 @@
-const express = require("express");
 const router = require("express").Router();
 const auth = require("../middlewares/auth");
 const { getUsers, getUser } = require("../controllers/users");
