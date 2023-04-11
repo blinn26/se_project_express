87e88285960b24003c5bf0af42ace66629479a6b
createdbybenlinn <benlinn26@gmail.com>
2023-04-11 16:00:08 -0700

    step 6

--- controllers/users.js
+++ controllers/users.js
@@ -109,8 +109,27 @@ const login = async (req, res) => {
       .json({ message: "Internal server error" });
   }
 };
+const getCurrentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.userId);
+
+    if (!user) {
+      return res
+        .status(ERROR_CODES.NOT_FOUND)
+        .send({ message: "User not found" });
+    }
+
+    res.status(ERROR_CODES.OK).send({ data: user });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(ERROR_CODES.INTERNAL_SERVER_ERROR)
+      .send({ message: "Internal server error" });
+  }
+};
 
 module.exports = {
+  getCurrentUser,
   getUsers,
   getUser,
   createUser,
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
+const express = require("express");
 const router = require("express").Router();
+const auth = require("../middlewares/auth");
 const { getUsers, getUser } = require("../controllers/users");
+const { getCurrentUser } = require("../controllers/users");
 
+router.get("/me", auth, getCurrentUser);
 router.get("/", getUsers);
 router.get("/:userId", getUser);
 
