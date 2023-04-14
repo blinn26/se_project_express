767ca4fec52429d7d4e9be5e130bde86cf5af197
createdbybenlinn <benlinn26@gmail.com>
2023-04-14 11:28:33 -0700

    fixed step 4

--- routes/users.js
+++ routes/users.js
@@ -11,8 +11,8 @@ const {
 } = require("../controllers/users");
 
 router.get("/", auth, getUsers);
-router.get("/_id", auth, getUser);
-router.post("/", createUser);
+/* router.get("/_id", auth, getUser);
+router.post("/", createUser);  */
 router.post("/login", auth, login);
 router.get("/me", auth, getCurrentUser);
 router.patch("/me", auth, updateUser);
