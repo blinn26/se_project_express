eb0349f4b857a310723b3e48368ac943dd0ced3a
createdbybenlinn <benlinn26@gmail.com>
2023-04-14 17:51:54 -0700

    409 save bad request to already exits

--- controllers/users.js
+++ controllers/users.js
@@ -46,7 +46,7 @@ const createUser = async (req, res) => {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res
-        .status(ERROR_CODES.BAD_REQUEST)
+        .status(ERROR_CODES.ALREADY_EXIST)
         .send({ message: "A user with this email already exists." });
     }
 
