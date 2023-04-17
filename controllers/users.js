0fb37491c09c310e40c76d295051e4124a58c62b
createdbybenlinn <benlinn26@gmail.com>
2023-04-17 11:13:43 -0700

    fixed the password issue

--- controllers/users.js
+++ controllers/users.js
@@ -83,7 +83,7 @@ const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }.select({ password }));
 
     if (!user) {
       return res
