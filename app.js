e49cc3f9e8f7e66c25e1c5c151ab05d08bcc2ee8
createdbybenlinn <benlinn26@gmail.com>
2023-05-13 07:30:21 -0700

    app save back

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.options("*", cors());
 app.use((req, res, next) => {
   const allowedOrigins = [
     "http://localhost:3000",
-    "https://wtwr.crabdance.com",
+    "https://api.wtwr.crabdance.com",
   ];
   const { origin } = req.headers;
   if (allowedOrigins.includes(origin)) {
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 });
 
 mongoose
-  .connect("mongodb://localhost:27017/wtwr_db")
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
   .then(() => {})
   .catch(() => {});
 
