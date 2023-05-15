3996fe65d42bee4c55439a87df7649cbbc726c75
createdbybenlinn <benlinn26@gmail.com>
2023-05-15 16:09:28 -0700

    commit console logsss

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Types;
 const { HTTP_ERRORS } = require("../utils/httpErrors");
 const ClothingItem = require("../models/clothingItem");
 const BadRequestError = require("../errorConstructors/badRequestError");
-const ForbiddenError = require("../errorConstructors/forbiddenError");
+
 const NotFoundError = require("../errorConstructors/notFoundError");
 
 const createItem = async (req, res, next) => {
+  console.log("createItem called with req.body:", req.body);
   const { userId } = req.user;
   const { name, weather, imageUrl } = req.body;
 
@@ -16,10 +19,13 @@ const createItem = async (req, res, next) => {
   const item = new ClothingItem({ name, weather, imageUrl, owner: userId });
   await item.save();
 
+  console.log("createItem successful, item created:", item);
+
   return res.status(HTTP_ERRORS.CREATED).json(item);
 };
 
 const getItems = async (req, res, next) => {
+  console.log("getItems called");
   const { userId } = req.user;
   const items = await ClothingItem.find();
 
@@ -28,30 +34,46 @@ const getItems = async (req, res, next) => {
     return { ...item.toObject(), isLiked };
   });
 
+  console.log("getItems successful, items retrieved:", itemsWithIsLiked);
+
   return res.status(HTTP_ERRORS.OK).send({ data: itemsWithIsLiked });
 };
 
-const deleteItem = async (req, res, next) => {
+async function deleteItem(req, res, next) {
+  const { userId } = req.user; // define userId here
   const { itemId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(itemId)) {
-    throw new BadRequestError("Invalid item ID");
+  if (!ObjectId.isValid(itemId)) {
+    return next(
+      new BadRequestError(
+        "Invalid path parameter: 'itemId' must be a single String of 12 bytes or a string of 24 hex characters"
+      )
+    );
   }
 
   const item = await ClothingItem.findById(itemId);
 
   if (!item) {
-    throw new NotFoundError("Item not found");
+    return next(new NotFoundError("Item not found"));
   }
 
-  if (String(item.owner) !== String(req.user.userId)) {
-    throw new ForbiddenError("Forbidden");
+  // Log both values to check their actual values
+  console.log("Owner: ", String(item.owner));
+  console.log("User ID: ", String(req.user.userId));
+
+  if (!item.owner.equals(userId)) {
+    return res
+      .status(403)
+      .json({ message: "You do not have permission to delete this item." });
   }
 
-  await ClothingItem.deleteOne({ _id: itemId });
-  return res.send({ message: "Item deleted" });
-};
+  await item.remove();
+
+  return res.status(204).send();
+}
+
 const likeItem = async (req, res, next) => {
+  console.log("likeItem called with req.params:", req.params);
   const { itemId } = req.params;
   const { userId } = req.user;
 
@@ -70,12 +92,15 @@ const likeItem = async (req, res, next) => {
   }
 
   const isLiked = item.likes.includes(userId);
+  console.log("likeItem successful, item updated:", item);
+
   return res
     .status(HTTP_ERRORS.OK)
     .send({ data: { ...item.toObject(), isLiked } });
 };
 
 const dislikeItem = async (req, res, next) => {
+  console.log("dislikeItem called with req.params:", req.params);
   const { itemId } = req.params;
   const { userId } = req.user;
 
@@ -94,6 +119,8 @@ const dislikeItem = async (req, res, next) => {
   }
 
   const isLiked = item.likes.includes(userId);
+  console.log("dislikeItem successful, item updated:", item);
+
   return res
     .status(HTTP_ERRORS.OK)
     .send({ data: { ...item.toObject(), isLiked } });
