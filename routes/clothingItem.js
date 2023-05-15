fce5c677f99f4b6b5a2694ee991d73833bd50463
createdbybenlinn <benlinn26@gmail.com>
2023-05-14 20:33:26 -0700

    JOOOIIII

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -8,22 +8,23 @@ const {
 } = require("../controllers/clothingItem");
 
 const auth = require("../middlewares/auth");
+const { validateId, validateCardBody } = require("../middlewares/validation");
 
 // CRUD METHODS
 
 // CREATE
-router.post("/", auth, createItem);
+router.post("/", auth, validateCardBody, createItem);
 
 // READ
 router.get("/", auth, getItems);
 
 // DELETE
-router.delete("/:itemId", auth, deleteItem);
+router.delete("/:itemId", auth, validateId, deleteItem);
 
 // LIKE
-router.put("/:itemId/likes", auth, likeItem);
+router.put("/:itemId/likes", auth, validateId, likeItem);
 
 // DISLIKE
-router.delete("/:itemId/likes", auth, dislikeItem);
+router.delete("/:itemId/likes", auth, validateId, dislikeItem);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 const auth = require("../middlewares/auth");
 const { getCurrentUser } = require("../controllers/users");
 const userController = require("../controllers/users");
+const { validateUser } = require("../middlewares/validation");
 
 router.get("/me", auth, getCurrentUser);
 
-router.patch("/me", userController.updateProfile);
+router.patch("/me", validateUser, userController.updateProfile);
 
 module.exports = router;
