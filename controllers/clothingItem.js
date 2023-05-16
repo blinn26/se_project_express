449072dcc4bee066eb1e3730feecc729c22725e0
createdbybenlinn <benlinn26@gmail.com>
2023-05-16 10:09:14 -0700

    clothing item update

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -1,115 +1,133 @@
 const mongoose = require("mongoose");
 
 const { ObjectId } = mongoose.Types;
-const { HTTP_ERRORS } = require("../utils/httpErrors");
 const ClothingItem = require("../models/clothingItem");
 const BadRequestError = require("../errorConstructors/badRequestError");
-
 const NotFoundError = require("../errorConstructors/notFoundError");
 
-const createItem = async (req, res, next) => {
+const createItem = (req, res, next) => {
   const { userId } = req.user;
   const { name, weather, imageUrl } = req.body;
 
   if (!name || !weather || !imageUrl) {
-    throw new BadRequestError("Missing required fields");
+    next(new BadRequestError("Missing required fields"));
+    return;
   }
 
   const item = new ClothingItem({ name, weather, imageUrl, owner: userId });
-  await item.save();
 
-  return res.status(HTTP_ERRORS.CREATED).json(item);
+  item
+    .save()
+    .then((savedItem) => {
+      res.status(201).json(savedItem);
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const getItems = async (req, res, next) => {
+const getItems = (req, res, next) => {
   const { userId } = req.user;
-  const items = await ClothingItem.find();
 
-  const itemsWithIsLiked = items.map((item) => {
-    const isLiked = item.likes.includes(userId);
-    return { ...item.toObject(), isLiked };
-  });
+  ClothingItem.find()
+    .then((items) => {
+      const itemsWithIsLiked = items.map((item) => {
+        const isLiked = item.likes.includes(userId);
+        return { ...item.toObject(), isLiked };
+      });
 
-  return res.status(HTTP_ERRORS.OK).send({ data: itemsWithIsLiked });
+      res.status(200).send({ data: itemsWithIsLiked });
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
-async function deleteItem(req, res, next) {
-  const { userId } = req.user; // define userId here
+const deleteItem = (req, res, next) => {
+  const { userId } = req.user;
   const { itemId } = req.params;
 
   if (!ObjectId.isValid(itemId)) {
-    return next(
+    next(
       new BadRequestError(
         "Invalid path parameter: 'itemId' must be a single String of 12 bytes or a string of 24 hex characters"
       )
     );
+    return;
   }
 
-  const item = await ClothingItem.findById(itemId);
+  ClothingItem.findById(itemId)
+    .then((item) => {
+      if (!item) {
+        next(new NotFoundError("Item not found"));
+      } else if (!item.owner.equals(userId)) {
+        res
+          .status(403)
+          .json({ message: "You do not have permission to delete this item." });
+      } else {
+        return item.remove();
+      }
+    })
+    .then(() => {
+      res.status(204).send();
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
 
-  if (!item) {
-    return next(new NotFoundError("Item not found"));
-  }
-
-  if (!item.owner.equals(userId)) {
-    return res
-      .status(403)
-      .json({ message: "You do not have permission to delete this item." });
-  }
-
-  await item.remove();
-
-  return res.status(204).send();
-}
-
-const likeItem = async (req, res, next) => {
+const likeItem = (req, res, next) => {
   const { itemId } = req.params;
   const { userId } = req.user;
 
-  if (!mongoose.Types.ObjectId.isValid(itemId)) {
-    throw new BadRequestError("Invalid item ID");
+  if (!ObjectId.isValid(itemId)) {
+    next(new BadRequestError("Invalid item ID"));
+    return;
   }
 
-  const item = await ClothingItem.findByIdAndUpdate(
+  ClothingItem.findByIdAndUpdate(
     itemId,
     { $addToSet: { likes: userId } },
     { new: true }
-  );
-
-  if (!item) {
-    throw new NotFoundError("Item not found");
-  }
-
-  const isLiked = item.likes.includes(userId);
-
-  return res
-    .status(HTTP_ERRORS.OK)
-    .send({ data: { ...item.toObject(), isLiked } });
+  )
+    .then((item) => {
+      if (!item) {
+        next(new NotFoundError("Item not found"));
+      } else {
+        const isLiked = item.likes.includes(userId);
+        res.status(200).send({ data: { ...item.toObject(), isLiked } });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const dislikeItem = async (req, res, next) => {
+const dislikeItem = (req, res, next) => {
   const { itemId } = req.params;
   const { userId } = req.user;
 
-  if (!mongoose.Types.ObjectId.isValid(itemId)) {
-    throw new BadRequestError("Invalid item ID");
+  if (!ObjectId.isValid(itemId)) {
+    next(new BadRequestError("Invalid item ID"));
+    return;
   }
 
-  const item = await ClothingItem.findByIdAndUpdate(
+  ClothingItem.findByIdAndUpdate(
     itemId,
     { $pull: { likes: userId } },
     { new: true }
-  );
-
-  if (!item) {
-    throw new NotFoundError("Item not found");
-  }
-
-  const isLiked = item.likes.includes(userId);
-
-  return res
-    .status(HTTP_ERRORS.OK)
-    .send({ data: { ...item.toObject(), isLiked } });
+  )
+    .then((item) => {
+      if (!item) {
+        next(new NotFoundError("Item not found"));
+      } else {
+        const isLiked = item.likes.includes(userId);
+        res.status(200).send({ data: { ...item.toObject(), isLiked } });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
 module.exports = {
