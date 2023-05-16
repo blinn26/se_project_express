822cff1f2df4aaaa1a1ad23251346c848fe88e3b
createdbybenlinn <benlinn26@gmail.com>
2023-05-16 09:54:41 -0700

    users.js and es

--- controllers/users.js
+++ controllers/users.js
@@ -6,103 +6,114 @@ const { JWT_SECRET } = require("../utils/config");
 const BadRequestError = require("../errorConstructors/badRequestError");
 const UnauthorizedError = require("../errorConstructors/unauthorizedError");
 const NotFoundError = require("../errorConstructors/notFoundError");
-const ConflictError = require("../errorConstructors/conflictError");
 
-const createUser = async (req, res, next) => {
+const createUser = (req, res, next) => {
   const { name, email, password, avatar } = req.body;
 
-  try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      throw new ConflictError("A user with this email already exists.");
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      name,
-      email,
-      password: hashedPassword,
-      avatar,
+  User.findOne({ email })
+    .then((existingUser) => {
+      if (existingUser) {
+        next(new BadRequestError("A user with this email already exists."));
+      } else {
+        return bcrypt
+          .hash(password, 10)
+          .then((hashedPassword) =>
+            User.create({
+              name,
+              email,
+              password: hashedPassword,
+              avatar,
+            })
+          )
+          .then((user) => {
+            const userObject = user.toObject();
+            delete userObject.password;
+            res.status(201).send(userObject);
+          });
+      }
+    })
+    .catch((error) => {
+      next(error);
     });
-
-    const userObject = user.toObject();
-    delete userObject.password;
-    res.status(201).send(userObject);
-  } catch (error) {
-    next(error);
-  }
 };
 
-const login = async (req, res, next) => {
+const login = (req, res, next) => {
   const { email, password } = req.body;
 
-  try {
-    const user = await User.findOne({ email }).select("+password");
+  User.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        next(new UnauthorizedError("User not found"));
+        return;
+      }
 
-    if (!user) {
-      throw new UnauthorizedError("User not found");
-    }
+      return bcrypt.compare(password, user.password).then((passwordMatches) => {
+        if (!passwordMatches) {
+          next(new UnauthorizedError("Invalid credentials"));
+        } else {
+          const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+            expiresIn: "7 days",
+          });
 
-    const passwordMatches = await bcrypt.compare(password, user.password);
-
-    if (!passwordMatches) {
-      throw new UnauthorizedError("Invalid credentials");
-    }
-
-    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
-      expiresIn: "7 days",
+          res.status(200).json({ token });
+        }
+      });
+    })
+    .catch((error) => {
+      next(error);
     });
-
-    res.status(200).json({ token });
-  } catch (error) {
-    next(error);
-  }
+};
+const getCurrentUser = (req, res, next) => {
+  User.findById(req.user.userId)
+    .then((user) => {
+      if (!user) {
+        next(new NotFoundError("User not found"));
+      } else {
+        res.status(200).send({ data: user });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 };
 
-const getCurrentUser = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.user.userId);
+const updateProfile = (req, res, next) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "avatar"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-    if (!user) {
-      throw new NotFoundError("User not found");
-    }
-
-    res.status(200).send({ data: user });
-  } catch (error) {
-    next(error);
+  if (!isValidOperation) {
+    next(new BadRequestError("Invalid updates!"));
+    return;
   }
-};
 
-const updateProfile = async (req, res, next) => {
-  try {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ["name", "avatar"];
-    const isValidOperation = updates.every((update) => {
-      return allowedUpdates.includes(update);
+  const { userId } = req.user;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        next(new NotFoundError("User not found"));
+      } else {
+        const updatedUserProps = { ...user._doc };
+        updates.forEach((update) => {
+          updatedUserProps[update] = req.body[update];
+        });
+
+        User.findByIdAndUpdate(userId, updatedUserProps, { new: true })
+          .then((updatedUserResult) => {
+            res.send({ data: updatedUserResult });
+          })
+          .catch((error) => {
+            next(error);
+          });
+      }
+    })
+    .catch((error) => {
+      next(error);
     });
-
-    if (!isValidOperation) {
-      throw new BadRequestError("Invalid updates!");
-    }
-
-    const { userId } = req.user;
-
-    const user = await User.findById(userId);
-    if (!user) {
-      throw new NotFoundError("User not found");
-    }
-
-    updates.forEach((update) => {
-      user[update] = req.body[update];
-    });
-
-    await user.save({ validateBeforeSave: true });
-
-    res.send({ data: user });
-  } catch (error) {
-    next(error);
-  }
 };
 
 module.exports = {
