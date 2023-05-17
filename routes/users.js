eeaca46530d602ccf86e6f69eb3ab7f186c76689
createdbybenlinn <benlinn26@gmail.com>
2023-05-17 10:22:38 -0700

    save corrrections

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 const auth = require("../middlewares/auth");
 const { getCurrentUser } = require("../controllers/users");
 const userController = require("../controllers/users");
+const { validateCardBody } = require("../middlewares/validation");
 
 router.get("/me", auth, getCurrentUser);
 
-router.patch("/me", userController.updateProfile);
+router.patch("/me", validateCardBody, userController.updateProfile);
 
 module.exports = router;
